d3f7d8d632afcbeb6dd688924d3e74c00249dcd8
lucianobaleani <lucianobaleani94@gmail.com>
2022-04-20 22:00:39 -0300

    [UPD] Creation of functions onchange and depends on vehicle.py and changes in xml files vehicle_views and vehicle_demo

--- vehicle_fleet/models/vehicle.py
+++ vehicle_fleet/models/vehicle.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 
-from datetime import timedelta
+from datetime import date 
 from dateutil import relativedelta
 
 class Vehicle(models.Model):
@@ -10,23 +10,44 @@ class Vehicle(models.Model):
     brand_model = fields.Char(string="vehicle Brand/Model")
     sale_price = fields.Float()
     sale_date = fields.Date(default=fields.Date.today)
-    quantity_service = fields.Integer(string="Total of Services")
+    quantity_service = fields.Integer(string="Total of Services",
+                                        compute="_compute_service",
+                                        store=True)
+
     measurement_unit = fields.Selection(
         selection=[("kilometers", "Kilometers (Km)"), ("mileage", "Mileage(Mi)")],
         copy=False,
     )
     distance = fields.Integer()
-    current_price = fields.Float()
+    current_price = fields.Float(compute="_compute_current_price",
+                                store=True)
 
-    @api.onchange("measurement_unit", "distance", "sale_price")
-    def _compute_current_price(self):
+    
+    @api.depends("sale_price","measurement_unit","distance") 
+    def _compute_current_price(self) -> None:
+        """
+        Compute the current price of the vehicle.
+        :param sale_price, measurement_unit, distance: "fleet.vehicle" record.
+        :writes current_: A float as result based on the sale_price, the distance and the measurement_unit(Km o Mi).
+        """
+        self.ensure_one()
         if self.measurement_unit == "kilometers":
-            reduction = self.distance / 10000
-            current_price = self.distance * (0, 5 * reduction)
+            reduction = self.distance // 10000
+            self.current_price = self.sale_price - (self.sale_price * (0.05 * reduction))
         else:
-            reduction = self.distance / 6214
-            current_price = self.distance * (0, 5 * reduction)
+            reduction = self.distance // 6213.712
+            self.current_price = self.sale_price - (self.sale_price * (0.05 * reduction))
 
-    @api.depends("sale_date", "quantity_service")
-    def _compute_services(self):
-        months = relativedelta.relativedelta(-self.sale_date)
+
+    @api.onchange("sale_date")
+    def _compute_service(self) ->None :
+        """
+        Compute the number of services that the vehicle has recieved, 1 every 6 months.
+        :param sale_date: "fleet.vehicle record.
+        :writes: An int as a result base on the calculation between today and sale_date.
+        """
+        self.ensure_one()
+        today = date.today()
+        for record in self:
+            month = relativedelta.relativedelta(today, record.sale_date)
+            record.quantity_service= (month.months + (12*month.years))//6
